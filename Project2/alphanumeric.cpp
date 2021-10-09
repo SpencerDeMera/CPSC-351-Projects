c3a40c539ad06e1e66068dd50a41f9e30ac4b7d3
Spencer5515 <spencer.demera@csu.fullerton.edu>
2021-10-09 13:59:39 -0700

    Update alphanumeric.cpp

---
+++ Project2/alphanumeric.cpp
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+char* phraseArr;
+
+void *alpha(void *arg) {
+    istringstream iss(phraseArr);
+
+    do {
+        string temp;
+
+        iss >> temp;
+
+        if (isalpha(temp[0])) {
+            cout << "alpha: " << temp << endl;
+        } else if (isdigit(temp[0])) {
+            // TODO
+        } else {
+            // TODO 
+        }
+    } while (iss);
+}
+
+void *numeric(void *arg) {
+    istringstream iss(phraseArr);
+
+    do {
+        string temp;
+
+        iss >> temp;
+
+        if (isdigit(temp[0])) {
+            cout << "numeric: " << temp << endl;
+        } else if (isalpha(temp[0])) {
+            // TODO
+        } else {
+           // TODO 
+        }
+    } while (iss);
+}
+
+int main (int argc, char *argv[]) {
+    // Error check for input
+    if (argc != 2) {
+        fprintf(stderr, "usage: ./<filename> <phrase>\n");
+        return -1;
+    }
+
+    phraseArr = argv[1];
+
+    pthread_t p1, p2;
+
+    pthread_create(&p1, NULL, alpha, (void*)phraseArr);
+    pthread_create(&p2, NULL, numeric, (void*)phraseArr);
+
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+
+    cout << "\nProgram Ending...\n";
+    return 0;
+}
