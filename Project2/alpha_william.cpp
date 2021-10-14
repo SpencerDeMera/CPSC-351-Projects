e6769aa895d7ea52c8a0ff585209a41a2fba28cb
William Pham Duong <wpduong@csu.fullerton.edu>
2021-10-14 11:20:51 -0700

    Update alpha_william.cpp

--- Project2/alpha_william.cpp
+++ Project2/alpha_william.cpp
@@ -4,99 +4,74 @@
 #include <pthread.h>
 #include <bits/stdc++.h>
 #include <iostream>
-#include <sstream>
 #include <sched.h>
 using namespace std;
 
+// Global variable
 string phraseArr;
 
+// Checks if a word in a string starts with a letter
 void *alpha(void *arg) {
-  string myPhrase = phraseArr;
+    string myPhrase = phraseArr;
 
-  string temp;
-  string temp2;
+    string temp;
+    string temp2;
 
-  // for (int i = 0; i < myPhrase.size(); i++) {
-  //   if (myPhrase[i] != ' ') {
-  //     temp += myPhrase[i];
-  //   }
-  //   else {
-  //     temp2 = temp;
-  //     temp = "";
-  //     if (isalpha(temp2[0])) {
-  //       cout << "alpha: " << temp2 << endl;
-  //     }
-  //     else {
-  //       sched_yield();
-  //       // cout << "yeet" << endl;
-  //     }
-  //   }
-  // }
-
-  for (auto x : myPhrase) {
-    if (x != ' ') {
-      temp += x;
-    }
-    else {
-      temp2 = temp;
-      temp = "";
-      if (isalpha(temp2[0])) {
-        cout << "alpha: " << temp2 << endl;
+    // Ranged-based for loop to determine the "words" in the string
+    for (auto x : myPhrase) {
+      if (x != ' ') {
+        temp += x;
       }
       else {
-        sched_yield();
-        // cout << "yeet" << endl;
+        temp2 = temp;
+        // Need to clear temp variable to allow a new word to be read in
+        temp = "";
+
+        // Checks if the first letter of temp2 is a letter
+        if (isalpha(temp2[0])) {
+          // If so, then print the word
+          cout << "alpha: " << temp2 << endl;
+        }
+        else {
+          // If not, yield CPU to next thread in queue
+          sched_yield();
+        }
       }
     }
-  }
 
-
-  pthread_exit(0);
+    pthread_exit(0);
 }
 
+// Checks if a word in a string starts with a number
 void *numeric(void *arg) {
+    string myPhrase = phraseArr;
 
-  // cout << "here" << endl;
-  string myPhrase = phraseArr;
+    string temp;
+    string temp2;
 
-  string temp;
-  string temp2;
-
-  // for (int i = 0; i < myPhrase.size(); i++) {
-  //   if (myPhrase[i] != ' ') {
-  //     temp += myPhrase[i];
-  //   }
-  //   else {
-  //     temp2 = temp;
-  //     temp = "";
-  //     if (isdigit(temp2[0])) {
-  //       cout << "numeric: " << temp2 << endl;
-  //     }
-  //     else {
-  //       sched_yield();
-  //       // cout << "yeet" << endl;
-  //     }
-  //   }
-  // }
-
-  for (auto x : myPhrase) {
-    if (x != ' ') {
-      temp += x;
-    }
-    else {
-      temp2 = temp;
-      temp = "";
-      if (isdigit(temp2[0])) {
-        cout << "numeric: " << temp2 << endl;
+    // Ranged-based for loop to determine the "words" in the string
+    for (auto x : myPhrase) {
+      if (x != ' ') {
+        temp += x;
       }
       else {
-        sched_yield();
-        // cout << "yeet" << endl;
+        temp2 = temp;
+        // Need to clear temp variable to allow a new word to be read in
+        temp = "";
+
+        // Checks if the first letter of temp2 is a number
+        if (isdigit(temp2[0])) {
+          // If so, then print the word
+          cout << "numeric: " << temp2 << endl;
+        }
+        else {
+          // If not, yield CPU to next thread in queue
+          sched_yield();
+        }
       }
     }
-  }
 
-  pthread_exit(0);
+    pthread_exit(0);
 }
 
 int main (int argc, char *argv[]) {
@@ -106,8 +81,14 @@ int main (int argc, char *argv[]) {
         return -1;
     }
 
+    // Assign input to global variable
     phraseArr = argv[1];
+    // Add a space to the end of the global variable to separate end string
+    // char from the last word
+    phraseArr += ' ';
 
+
+    // Taken from concurrent threads examples
     pthread_t p1, p2;
 
     pthread_create(&p1, NULL, alpha, NULL);
@@ -117,6 +98,7 @@ int main (int argc, char *argv[]) {
     pthread_join(p2, NULL);
 
 
+    // Finish program
     cout << "\nProgram Ending...\n";
     return 0;
 }
